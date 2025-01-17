d06667f39c510bfdc51af022832a81b019c0ea3e
DongHoon <ehdgnsdlrk@gmail.com>
2025-01-17 19:43:24 +0900

    2025 01 17 캡슐화 상속 깊은복사

---
+++ Class 4th(Shallow Copy & Deep Copy)/Class 4th(Shallow Copy & Deep Copy).cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+class Terran
+{
+protected:
+    int health = 100;
+    int defense= 30;
+public:
+    Terran()
+    {
+        cout << "Created Terran" << endl;
+    }
+};
+
+class Marine : public Terran
+{
+private:
+    int attack =59;
+public:
+    Marine()
+    {
+        cout << "Created Marine" << endl;
+
+    }
+    int Health()
+    {
+        return health;
+    }
+    int Defense()
+    {
+        return defense;
+    }
+    int Attack()
+    {
+        return attack;
+    }
+};
+
+
+int main()
+{
+#pragma region 캘슐화
+    // 클래스 안에 서로 연관되어 있는 속성과 기능들을 하나의 캡술로 
+    // 만들어 데이터를 외부로부터 보호하며, 사용자에게 인터페이스를
+    // 제공하여 클래스의 기능을 사용하게 하는 것입니다.
+    
+#pragma endregion
+    
+#pragma region 상속
+    // 상위 클래스의 속성을 하위 클래스가 사용할 수 있도록
+    // 설정해주는 기능입니다.
+    Terran terran;
+
+    Marine marine;
+    
+    cout << "마린의 체력: " << marine.Health() << endl;
+    cout << "마린의 공격력: " << marine.Attack() << endl;
+    cout << "마린의 방어력: " << marine.Defense() << endl;
+
+    // 클래스의 상속 관계에서 상위 클래스는 하위 클래스의 
+    // 속성을 사용 할 수 없으며, 하위 클래스는 상위 클래스의 
+    // 메모리가 포함된 상태로 메모리의 크기가 결정됩니다.
+
+#pragma endregion
+
+    return 0;
+}
+
--- Class3 th (Constructor & Destructor)/Class3 th (Constructor & Destructor).cpp
+++ Class3 th (Constructor & Destructor)/Class3 th (Constructor & Destructor).cpp
@@ -26,6 +26,10 @@ public:
 
     Unit(const Unit & clone)
     {
+        // 얕은 복사는 객체가 메모리에서 해제될 때 생성자
+        // 의 실행 순서와 반대로 소멸자가 실행되기 때문에
+        // 복사한 객체가 먼저 해제되므로 원래 잇던 객체가
+        // 해제될 때는 이미 해제된 메모리에 접근하게 됩니다.
         health = clone.health;
 
         kill = clone.kill;
@@ -42,18 +46,42 @@ public:
 
 };
 
+class Item {
+private:
+    int * price;
+    char grade;
+public:
+    Item(char grade)
+    {
+        this->grade = grade;
+
+        price = new int;
+    }
+    Item(const Item & clone)
+    {
+        grade = clone.grade;
+
+        price = new int;
+    }
+
+    ~Item()
+    {
+        delete price;
+    }
+};
+
 int main()
 {
 #pragma region 복사 생성자
     // 같은 객체를 복사하여 생성시킬 때 호출되는
     // 생성자 입니다.
-    Unit marine(40);
-
-    Unit marine1(marine);
-
-    cout << marine.Health() << endl;
-    cout << marine1.Health() << endl;
-
+    //Unit marine(40);
+    //
+    //Unit marine1(marine);
+    //
+    //cout << marine.Health() << endl;
+    //cout << marine1.Health() << endl;
+    //
     // 복사 생성자를 정의하지 않고, 객체를 복사하게 되면
     // 기본 복사 생성자가 호출되기 때문에 얕은 복사로 객체가 복사됩니다.
 
@@ -74,15 +102,27 @@ int main()
     cout << "prt 2: " << ptr1 << endl;
 
 
-    Unit firebat1(50);
+    //Unit firebat1(50);
 
-    Unit firebat2(firebat1); //터짐
+    //Unit firebat2(firebat1); 
 
     // 얕은 복사의 경우 같은 객체가 서로 같은 메모리
     // 공간을 참조하고 있기 때문에 하나의 객체로 값을
     // 변경하게 되면 서로 참조된 객체도 함께 영향을 받습니다.
 #pragma endregion
 
+#pragma region 깊은 복사
+    // 객체를 복사할 때, 참조 값이 아닌 객체 자체를 새로
+    // 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+
+    Item posison1('B');
+
+    Item posison2(posison1);
+
+
+    
+
+#pragma endregion
 
     return 0;
 }
