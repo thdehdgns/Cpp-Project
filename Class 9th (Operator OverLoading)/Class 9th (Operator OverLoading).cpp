24314d5e9966219db7209fb82345374ba925d51f
DongHoon <ehdgnsdlrk@gmail.com>
2025-02-03 19:45:21 +0900

    2025.02.03 스마트 포인터

--- Class 9th (Operator OverLoading)/Class 9th (Operator OverLoading).cpp
+++ Class 9th (Operator OverLoading)/Class 9th (Operator OverLoading).cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
 #include "List.h"
+#include <string>
 using namespace std;
+template <typename T>
+bool Same(T a, T b)
+{
+	return a == b;
+}
+template <typename T>
+bool Same(string a, string b)
+{
+	return a.length() == b.length();
+	
+}
 int main()
 {
 #pragma region 템플릿
@@ -8,10 +20,35 @@ int main()
 	// 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수
 	// 있는 기능입니다.
 
-	List<int> intList;
+	// List<int> intList;
+	// 
+	// intList.Resize(5);
+	// intList.Add(1);
+	// intList.Add(6);
+	// intList.Add(4);
+	// intList.Add(7);
+	// intList.Add(3);
+	// intList.Add(2);
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 	cout << intList[i] << endl;
+	// }
+	
 
 #pragma endregion
 
+#pragma region 템플릿 특수화
+	// 특정 자료형에 대해 다르게 처리하고 싶을 경우 
+	// 특정한 자료형만 다른 형식으로 동작시키는 템플릿 기능입니다.
+	
+	
+	cout << Same(1, 2) << endl;
+	cout << Same('A', 'B') << endl;
+	cout << Same('A', 'A') << endl;
+	cout << Same("aaa", "bbb") << endl;
+
+#pragma endregion
 
 
 	return 0;
--- Class 9th (Operator OverLoading)/List.h
+++ Class 9th (Operator OverLoading)/List.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 template <typename T>
 class List
 {
@@ -8,5 +10,51 @@ private:
 	T* pointer;
 
 public:
+	List() {
+		index = 0;
+		size = 0;
+		pointer = nullptr;
+	}
+	void Resize(int size)
+	{
+		this->size = size;
+		pointer = new T[this->size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			pointer[i] = 0;
+		}
+			
+	}
+	void Add(T data)
+	{
+		if (index >= size)
+		{
+			cout << "Index Out of Range" << endl;
+		}
+		else if(size <= 0)
+		{
+			cout << "Null index size" << endl;
+		}
+		else
+		{
+			pointer[index++] = data;
+		}
+		
+	}
+
+	const T & operator [](int index)
+	{
+		return pointer[index];
+	}
+	
+
+
+	~List() {
+		if(pointer != nullptr)
+		delete [] pointer;
+		
+	}
 };
+
 
