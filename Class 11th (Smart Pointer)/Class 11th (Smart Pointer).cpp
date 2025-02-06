d8bcf0876903f7bbee14d69756abf4422ab843e9
DongHoon <ehdgnsdlrk@gmail.com>
2025-02-06 19:42:02 +0900

    2025-02-06

--- Class 11th (Smart Pointer)/Class 11th (Smart Pointer).cpp
+++ Class 11th (Smart Pointer)/Class 11th (Smart Pointer).cpp
@@ -1,9 +1,36 @@
 ﻿#include <iostream>
+#include <thread>
 using namespace std;
+
+void Input()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Input.." << endl;
+    }
+}
+
+void Randerer()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Renderer.." << endl;
+    }
+}
+
+
 int main()
 {
 #pragma region 스레드
     // 프로세스 내에서 실행되는 하나의 작업 단위입니다.
+
+    // std::thread thread1(Input);
+    // std::thread thread2(Randerer); 
+    // 
+    // thread1.join();
+    // thread2.join();
+
+
 #pragma endregion
 
 
---
+++ Class 12th(Thread)/Class 12th(Thread).cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+#pragma region vector container
+    
+    vector<int> vector;
+    vector.reserve(6);
+    vector.push_back(10);
+    vector.push_back(20);
+    vector.push_back(30);
+    vector.push_back(40);
+    vector.push_back(50);
+    
+    for (int i = 0; i < vector.size(); i++)
+    {
+        cout << vector[i] << endl;
+    }
+    vector.pop_back();
+    vector.pop_back();
+    cout << "size:" << vector.size() << endl;
+    cout << "capacity:" << vector.capacity() << endl;
+
+    for (int i = 0; i < vector.size(); i++)
+    {
+        cout << vector[i] << endl;
+    }
+
+#pragma endregion
+
+    
+
+
+    return 0;
+}
+
+
